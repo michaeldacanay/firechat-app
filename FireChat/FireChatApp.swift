b83c5973cada9421575a64749d00b988db135371
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-23 18:47:21 -0400

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Michael Dacanay on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
