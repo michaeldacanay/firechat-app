bcddd54837d40e9c9ea80494851f47e42d655000
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-23 19:06:15 -0400

    step 1: initialize project with firebase

--- FireChat/FireChatApp.swift
+++ FireChat/FireChatApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct FireChatApp: App {
+    init() { // <-- Add an init
+        FirebaseApp.configure() // <-- Configure Firebase app
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
