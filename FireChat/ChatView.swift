b7c909aa21bb0ee55b0ab80db90a474b14ab982e
Michael Dacanay <michaeldacanayusa@gmail.com>
2024-03-27 10:29:32 -0400

    step 8: display and style the chat messages

--- FireChat/ChatView.swift
+++ FireChat/ChatView.swift
@@ -20,17 +20,67 @@ struct ChatView: View {
     
     var body: some View {
         NavigationStack {
-            Text("Welcome to FireChat!")
-                .navigationTitle("Chat")
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem {
-                        Button("Sign out") {
-                            authManager.signOut()
-                        }
+            
+            ScrollView { // <-- Add ScrollView
+                VStack { // <-- Add VStack
+                    ForEach(messageManager.messages) { message in // <-- Add ForEach, accessing the messages property of the message manager
+                        MessageRow(text: message.text, isOutgoing: authManager.userEmail == message.username) // <-- Display the text for each message
                     }
                 }
+            }
+            .defaultScrollAnchor(.bottom)
+            .navigationTitle("Chat")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem {
+                    Button("Sign out") {
+                        authManager.signOut()
+                    }
+                }
+            }
+            
+//            Text("Welcome to FireChat!")
+//                .navigationTitle("Chat")
+//                .navigationBarTitleDisplayMode(.inline)
+//                .toolbar {
+//                    ToolbarItem {
+//                        Button("Sign out") {
+//                            authManager.signOut()
+//                        }
+//                    }
+//                }
         }
+    }
+}
+
+struct MessageRow: View {
+    let text: String // <-- The message text
+    let isOutgoing: Bool // <-- true if sent by the current user
+
+    var body: some View {
+        HStack {
+            if isOutgoing {
+                Spacer() // <-- Spacer shifts message bubble to the right for outgoing messages
+            }
+            messageBubble // <-- The message bubble
+            if !isOutgoing {
+                Spacer() // <-- Spacer shifts message bubble to the left for incoming messages
+            }
+        }
+    }
+
+    private var messageBubble: some View {
+        Text(text)
+            .fixedSize(horizontal: false, vertical: true) // <-- Allow message text to expand and wrap vertically if needed
+            .foregroundStyle(isOutgoing ? .white : .primary) // <-- Set the text color based on outgoing status
+            .padding(.vertical, 10)
+            .padding(.horizontal, 12)
+            .background(
+                RoundedRectangle(cornerRadius: 20.0) // <-- Add bubble shape to the background
+                    .fill(isOutgoing ? Color.blue.gradient : Color(.systemGray5).gradient) // <-- Set bubble color based on outgoing status
+            )
+            .padding(isOutgoing ? .trailing : .leading, 12) // <-- Set padding based on outgoing status
+            .containerRelativeFrame(.horizontal, count: 7, span: 5, spacing: 0, alignment: isOutgoing ? .trailing : .leading) // <-- Set message size relative to container (the scroll view width in this case)
     }
 }
 
